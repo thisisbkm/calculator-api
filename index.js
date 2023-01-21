f4f242c5c8b049518f322844bf6a3316a115562a
thisisbkm <bharatkumarmareedu@gmail.com>
2023-01-21 14:40:37 +0530

    first

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 4001;
+app.use(express.json());
+app.get("/sum", (request, response) => {
+    let a = 0;
+    Object.entries(request.body).forEach(([key,value])=>{
+        a = a+value;
+    });
+    if(isNaN(a)){
+        response.send("Json Parameters : Unlimited key value pairs\nNo restriction on keys\nValues should be numbers");
+    }else{
+        response.send(`${a}`);
+    }
+})
+
+app.get("/subtract", (request, response) => {
+    let a = request.body.num1;
+    let b = request.body.num2;
+    let c = a - b;
+    if(isNaN(c)){
+        response.send("Json Parameters : 2\nKey1 : num1, key2: num2");
+    }else{
+        response.send(`${c}`);
+    }
+    // console.log(a);
+})
+app.get("/product", (request, response) => {
+    let a = 1;
+    Object.entries(request.body).forEach(([key,value])=>{
+        a = a*value;
+    });
+    if(isNaN(a)){
+        response.send("Json Parameters : Unlimited key value pairs\nNo restriction on keys\nValues should be numbers");
+    }else{
+        response.send(`${a}`);
+    }
+})
+
+app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
